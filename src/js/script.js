bdc31558dcef66d89ddc9928835bad2127c11cf0
ovsundal <osundal83@gmail.com>
2017-08-05 11:51:28 +0200

    add google map, center on Stavanger

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,20 @@
-<!-- Menu Toggle Script -->
+$( document ).ready(function() {
 
+<!-- Menu Toggle Script -->
 $("#menu-toggle").click(function(e) {
     e.preventDefault();
     $("#wrapper").toggleClass("toggled");
+});
+
+function initMap() {
+    let stavanger = {lat: 58.9700, lng: 5.7331};
+    let map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: stavanger
+    });
+}
+
+//initialize function
+initMap();
+
 });
