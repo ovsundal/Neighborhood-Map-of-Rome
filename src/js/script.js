6e18abedc2645fc40fdd9eced6e35472ee1e7b44
ovsundal <osundal83@gmail.com>
2017-08-05 22:48:08 +0200

    add 5 markers of dining places to map

--- src/js/script.js
+++ src/js/script.js
@@ -6,12 +6,45 @@ $("#menu-toggle").click(function(e) {
     $("#wrapper").toggleClass("toggled");
 });
 
+    let markerContainer = [
+        {
+            "position": {lat: 58.970936, lng: 5.732062},
+            "title": 'Døgnvill'
+        },
+        {
+            "position": {lat: 58.970818, lng: 5.735233},
+            "title": 'Steam Kaffebar'
+        },
+        {
+            "position": {lat: 58.970135, lng: 5.736521},
+            "title": 'Thai Nong Khai As'
+        },
+        {
+            "position": {lat: 58.971349, lng: 5.738650},
+            "title": 'Mogul India'
+        },
+        {
+            "position": {lat: 58.972237, lng: 5.732062},
+            "title": 'Delhi Bar & Restaurant'
+        },
+    ];
+
 function initMap() {
     let stavanger = {lat: 58.9700, lng: 5.7331};
     let map = new google.maps.Map(document.getElementById('map'), {
         zoom: 15,
         center: stavanger
     });
+
+    //add markers to map
+    for (let i = 0; i < markerContainer.length; i++) {
+        let marker = new google.maps.Marker({
+            position: markerContainer[i].position,
+            map: map,
+            title: markerContainer[i].title
+        });
+    }
+
 }
 
 //initialize function
