97feb7b77ad5d94ac347dc6dd33fbb1aeb8b7a5b
Ove Sundal <osu002@post.uit.no>
2017-08-11 22:52:24 +0200

    add maps to main window

--- src/js/script.js
+++ src/js/script.js
@@ -62,6 +62,46 @@ ko.applyBindings(new ViewModel());
 
 
 
+// function initMap() {
+// //
+//     //create map
+//     let stavanger = {lat: 58.9700, lng: 5.7331};
+//     let map = new google.maps.Map(document.getElementById('map'), {
+//         zoom: 15,
+//         center: stavanger
+//     });
+//
+//     initMap();
+//
+//         //QUESTION: Why is this loop run twice?
+//     //add markers to map
+//     for (let i = 0; i < markerContainer().length; i++) {
+//         let marker = new google.maps.Marker({
+//             position: markerContainer()[i].position,
+//             map: map,
+//             title: markerContainer()[i].title
+//         });
+//
+//     //add infowindows to markers
+//         let infoWindow = new google.maps.InfoWindow({
+//             content: markerContainer()[i].title
+//         });
+//
+//         marker.addListener('click', function() {
+//             infoWindow.open(map, marker);
+//         });
+//
+//         //append marker location to listview
+//         $(".sidebar-nav").append('<li><a href="#">' + markerContainer()[i].title + '</a></li>');
+//     }
+// }
+
+
+
+
+
+
+
 
 // hard coded Array of location objects
 // https://github.com/udacity/ud864/blob/master/Project_Code_5_BeingStylish.html#L150
@@ -69,24 +109,10 @@ ko.applyBindings(new ViewModel());
 // initMap function (later)
 // https://developers.google.com/maps/documentation/javascript/examples/map-simple
 
-// Location constructor similiar to the Cat constructor form the JavaScript Design Patterns course (optional)
-
-// ViewModel constructor
-// http://knockoutjs.com/documentation/observables.html#mvvm-and-view-models
-// In the ViewmModel create an observableArray with location objects
-// this.locations = ko.observableArray(locations); // if you do not want to use a Location constructor
-// Separating Out the Model video lesson:
-// https://classroom.udacity.com/nanodegrees/nd001/parts/e87c34bf-a9c0-415f-b007-c2c2d7eead73/modules/271165859175461/lessons/3406489055/concepts/34284402380923
-// Adding More Cats video lesson
-// https://classroom.udacity.com/nanodegrees/nd001/parts/e87c34bf-a9c0-415f-b007-c2c2d7eead73/modules/271165859175461/lessons/3406489055/concepts/34648186930923
-
 // Instantiate the ViewModel
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/new
 // The difference between defining the ViewModel as a function expression or defining the viewModel as an object literal:
 // https://discussions.udacity.com/t/text-not-updating-with-search-box/182886/6
-
-// Apply the bindings aka activate KO
-// http://knockoutjs.com/documentation/observables.html#mvvm-and-view-models#activating-knockout
 
 
 // $( document ).ready(function() {
